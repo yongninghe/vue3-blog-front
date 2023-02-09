79faf827693ecd381fb116e5843a88579f32b2b5
yongninghe <yongninghe@163.com>
2023-02-09 16:12:16 +0800

    添加页脚

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,18 +3,20 @@
     <el-container>
       <el-header><BlogHeader msg="博客列表"> </BlogHeader></el-header>
       <el-main>Main</el-main>
-      <el-footer>Footer</el-footer>
+      <el-footer style="alignment: center"><BlogFooter></BlogFooter></el-footer>
     </el-container>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BlogHeader from '@/components/BlogHeader.vue'; // @ is an alias to /src
+import BlogFooter from '@/components/BlogFooter.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
     BlogHeader,
+    BlogFooter
   },
 });
 </script>
