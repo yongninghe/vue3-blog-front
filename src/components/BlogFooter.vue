8aeeac4269a41e292d29c49684fb7ab7c4f58833
yongninghe <yongninghe@163.com>
2023-02-09 16:12:08 +0800

    添加页脚

---
+++ src/components/BlogFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="example-pagination-block" style="alignment: center">
+    <el-pagination layout="prev, pager, next" :total="1000" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BlogFooter',
+  props: {
+    msg: String,
+  },
+});
+</script>
+<style scoped>
+.example-pagination-block + .example-pagination-block {
+  margin-top: 10px;
+  align-content: center;
+  alignment: center;
+}
+.example-pagination-block .example-demonstration {
+  margin-bottom: 16px;
+  align-content: center;
+  alignment: center;
+}
+</style>
