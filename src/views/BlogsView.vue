89be9e0450116c2b10eda73cf8e857fdc4156a48
yongninghe <yongninghe@163.com>
2023-02-12 09:56:39 +0800

    修改博客内容component名称

--- src/views/BlogsView.vue
+++ src/views/BlogsView.vue
@@ -5,7 +5,7 @@
         <BlogHeader msg="博客列表"></BlogHeader>
       </el-header>
       <el-main>
-        <BlogContent :blog-items="blogItems"></BlogContent>
+        <BlogItem :blog-items="blogItems"></BlogItem>
       </el-main>
       <el-footer>
         <BlogFooter></BlogFooter>
@@ -16,22 +16,32 @@
 <script lang="ts">
 import {ref, toRefs, reactive, defineComponent} from 'vue'
 import BlogHeader from '@/components/BlogHeader.vue'; // @ is an alias to /src
-import BlogContent from '@/components/BlogContent.vue';
+import BlogItem from '@/components/BlogItem.vue';
 import BlogFooter from '@/components/BlogFooter.vue';
 
 export default defineComponent({
   name: 'BlogsView',
   components: {
     BlogHeader,
-    BlogContent,
+    BlogItem,
     BlogFooter
   },
   setup() {
     const blogItems = ref([{
       date: '2016-05-03',
-      title: '这是第一篇博客',
-      abstract: '这是内容摘要',
-    }])
+      title: '这是第1篇博客',
+      abstract: '这是内容摘要1',
+    },
+      {
+        date: '2017-05-03',
+        title: '这是第2篇博客',
+        abstract: '这是内容摘要2',
+      },
+      {
+        date: '2018-05-03',
+        title: '这是第3篇博客',
+        abstract: '这是内容摘要3',
+      }])
     return {
       blogItems: blogItems,
     }
