e1fe378a44c1aed88ba8b942b793e8bd33165df0
yongninghe <yongninghe@163.com>
2023-02-12 12:15:16 +0800

    修改分页

--- src/components/BlogFooter.vue
+++ src/components/BlogFooter.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="example-pagination-block" style="alignment: center">
-    <el-pagination layout="prev, pager, next" :total="1000"/>
+    <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="10"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400">
+    </el-pagination>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, ref} from 'vue';
 
 export default defineComponent({
   name: 'BlogFooter',
   props: {
     msg: String,
   },
-});
+  setup(){
+    const currentPage = ref(4);
+    const handleSizeChange = (id: number) => {
+    alert("handleSizeChange" + id);
+  };
+    const handleCurrentChange = (id: number) => {
+      alert("handleCurrentChange" + id);
+    };
+  return {
+    handleSizeChange,
+    handleCurrentChange,
+    currentPage
+  }
+}
+})
+;
 </script>
 <style scoped>
 .example-pagination-block + .example-pagination-block {
